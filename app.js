64ea3d46ee63467bc78866112b2e35a0b09303ad
KALVIUM23 <rockstarsagarkv206@gmail.com>
2025-02-10 15:38:49 +0530

    done

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
-const express=require('express');
-const app=express();
+const express = require("express");
+
+const app = express();
 require("dotenv").config();
 app.use(express.json());
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('welcome to the world of of full stack development');
 })
 
-app.get("/users",async(req,res)=>{
-    try{
-        res.status(200).json({message:"recieved data"});
-    }catch(err){
-        res.status(500).json({message:"internal server error"})
+app.get("/users", async (req, res) => {
+    try {
+        res.status(200).json({ message: "recieved data" });
+    } catch (err) {
+        res.status(500).json({ message: "internal server error" })
     }
 
 });
 
-app.post("/users",async(req,res)=>{
-    try{
-        let{username,email,password,dateofbirth}=req.body;
+app.post("/users", async (req, res) => {
+   console.log(req.body);
+    try {
+        let { username, email, password, dateofbirth } = req.body;
 
-        if (!username || !email || !password || !dateofbirth){ 
-            return res.status(400).json({message:"please fill all the fields"})
-        };
+        if (!username || !email || !password || !dateofbirth) {
+            return res.status(400).json({ message: "please fill all the fields" })
+        }
+        else if (!username) {
+            return res.status(400).json({ message: "username cannot be empty" })
+        }
 
-        ifelse(!username){   
-            return res.status(400).json({message:"username cannot be empty"})
-        };
-        
-        ifelse(!email){
-            return res.status(400).json({message:"email cannot be empty"})
-        };
-        
-        ifelse(password.length<8 || password.length>16){ 
-            return res.status(400).json({message:"password should in range between 8 to 16"})
+        else if (!email) {
+            return res.status(400).json({ message: "email cannot be empty" })
         }
-        ifelse(!Dateofbirth)
-{             return res.status(400).json({message:"date of birth is required to fill"})
+        else if (password.length < 8 || password.length > 16) {
+            return res.status(400).json({ message: "password should be in range between 8 to 16" })
         }
-        res.status(200).json({message:`daa received as name :{name}, email:{email}, password:{password}, dateofbirth:{dateofbirth}`});
+        else if (!dateofbirth) {
+            return res.status(400).json({ message: "date of birth is required to fill" })
+        }
+        res.status(200).json({ message: `data received as name: ${username}, email: ${email}, password: ${password}, date of birth: ${dateofbirth}` });
     }
-    catch(error){
-        res.status(500).json({message:"internal server error"});
+    catch (error) {
+        res.status(500).json({ message: "internal server error" });
 
     }
 });
 
-let PORT=process.env.PORT || 5050;
-app.listen(PORT,()=>{
+let PORT = process.env.PORT || 5050;
+app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 })
